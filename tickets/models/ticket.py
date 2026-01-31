ce3d18f87c7d6bccca851b69292694318bcb96ba
Patrick Dunham <patrick.dunham@kcl.ac.uk>
2026-01-31 13:44:21 +0000

    Edit Ticket model for readability

--- tickets/models/ticket.py
+++ tickets/models/ticket.py
@@ -10,60 +10,46 @@ User = get_user_model()
 class Ticket(models.Model):
     """Model representing a student ticket."""
 
-    # FACULTIES
-    FOLSM = ("folsm", "Faculty of Life Sciences & Medicine (FoLSM)")
-    SSPP = ("sspp", "Faculty of Social Science & Public Policy (SSPP)")
-    NMPC = (
-        "nmpc",
-        "Florence Nightingale Faculty of Nursing, Midwifery & Palliative Care (NMPC)",
-    )
-    NMES = ("nmes", "Faculty of Natural, Mathematical & Engineering Sciences (NMES)")
-    AH = ("ah", "Faculty of Arts & Humanities (A&H)")
-    KBS = ("kbs", "King's Business School (KBS)")
-    DOCS = ("docs", "Faculty of Dentistry, Oral & Craniofacial Sciences (DOCS)")
-    DPSOL = ("dpsol", "The Dickson Poon School of Law (DPSoL)")
-    IOPPN = ("ioppn", "Institute of Psychiatry, Psychology & Neuroscience (IoPPN)")
-    FACULTIES = [FOLSM, SSPP, NMPC, NMES, AH, KBS, DOCS, DPSOL, IOPPN]
+    class Faculty(models.TextChoices):
+        FOLSM = "folsm", "Faculty of Life Sciences & Medicine (FoLSM)"
+        SSPP = "sspp", "Faculty of Social Science & Public Policy (SSPP)"
+        NMPC = (
+            "nmpc",
+            "Florence Nightingale Faculty of Nursing, Midwifery & Palliative Care (NMPC)",
+        )
+        NMES = "nmes", "Faculty of Natural, Mathematical & Engineering Sciences (NMES)"
+        AH = "ah", "Faculty of Arts & Humanities (A&H)"
+        KBS = "kbs", "King's Business School (KBS)"
+        DOCS = "docs", "Faculty of Dentistry, Oral & Craniofacial Sciences (DOCS)"
+        DPSOL = "dpsol", "The Dickson Poon School of Law (DPSoL)"
+        IOPPN = "ioppn", "Institute of Psychiatry, Psychology & Neuroscience (IoPPN)"
 
-    # STUDY LEVEL
-    UNDERGRADUATE = ("undergraduate", "Undergraduate")
-    POSTGRADUATE_TAUGHT = ("postgraduate_taught", "Postgraduate Taught")
-    POSTGRADUATE = ("postgraduate", "Postgraduate")
-    STUDY_LEVEL = [UNDERGRADUATE, POSTGRADUATE_TAUGHT, POSTGRADUATE]
+    class StudyLevel(models.TextChoices):
+        UNDERGRADUATE = "undergraduate", "Undergraduate"
+        POSTGRADUATE_TAUGHT = "postgraduate_taught", "Postgraduate Taught"
+        POSTGRADUATE = "postgraduate", "Postgraduate"
 
-    # CATEGORIES
-    ASSESSMENT = ("assessment", "Assessment")
-    WELFARE = ("welfare", "Welfare")
-    HEALTH = ("health", "Health")
-    CAREERS = ("careers", "Careers")
-    FINANCIAL_ISSUES = ("financial_issues", "Financial Issues")
-    UNI_PROCEDURES_REGULATIONS = (
-        "uni_procedures_regulations",
-        "University Procedures and Regulations",
-    )
-    MISCONDUCT_ALLEGATIONS = ("misconduct_allegations", "Misconduct Allegations")
-    OTHER = ("other", "Other")
-    CATEGORIES = [
-        ASSESSMENT,
-        WELFARE,
-        HEALTH,
-        CAREERS,
-        FINANCIAL_ISSUES,
-        UNI_PROCEDURES_REGULATIONS,
-        MISCONDUCT_ALLEGATIONS,
-        OTHER,
-    ]
+    class Category(models.TextChoices):
+        ASSESSMENT = "assessment", "Assessment"
+        WELFARE = "welfare", "Welfare"
+        HEALTH = "health", "Health"
+        CAREERS = "careers", "Careers"
+        FINANCIAL_ISSUES = "financial_issues", "Financial Issues"
+        UNI_PROCEDURES_REGULATIONS = (
+            "uni_procedures_regulations",
+            "University Procedures and Regulations",
+        )
+        MISCONDUCT_ALLEGATIONS = "misconduct_allegations", "Misconduct Allegations"
+        OTHER = "other", "Other"
 
-    # STATUS
-    AWAITING_STAFF = ("awaiting_staff", "Awaiting staff")
-    AWAITING_STUDENT = ("awaiting_student", "Awaiting student")
-    CLOSED = ("closed", "Closed")
-    STATUS = [AWAITING_STAFF, AWAITING_STUDENT, CLOSED]
+    class Status(models.TextChoices):
+        AWAITING_STAFF = "awaiting_staff", "Awaiting staff"
+        AWAITING_STUDENT = "awaiting_student", "Awaiting student"
+        CLOSED = "closed", "Closed"
 
-    # CLOSED REASONS
-    ANSWERED = ("answered", "Answered")
-    INACTIVITY = ("inactivity", "Inactivity")
-    CLOSED_REASONS = [ANSWERED, INACTIVITY]
+    class ClosedReason(models.TextChoices):
+        ANSWERED = "answered", "Answered"
+        INACTIVITY = "inactivity", "Inactivity"
 
     student = models.ForeignKey(
         User,
@@ -80,9 +66,9 @@ class Ticket(models.Model):
         limit_choices_to={"user_type": User.USER_TYPE_STAFF},
     )
 
-    faculty = models.CharField(max_length=64, choices=FACULTIES)
-    study_level = models.CharField(max_length=64, choices=STUDY_LEVEL)
-    category = models.CharField(max_length=64, choices=CATEGORIES)
+    faculty = models.CharField(max_length=64, choices=Faculty.choices)
+    study_level = models.CharField(max_length=64, choices=StudyLevel.choices)
+    category = models.CharField(max_length=64, choices=Category.choices)
 
     subject = models.CharField(max_length=78)
 
@@ -91,9 +77,11 @@ class Ticket(models.Model):
         max_length=BODY_MAX_LENGTH, validators=[MaxLengthValidator(BODY_MAX_LENGTH)]
     )
 
-    status = models.CharField(max_length=32, choices=STATUS, default=AWAITING_STAFF[0])
+    status = models.CharField(
+        max_length=32, choices=Status.choices, default=Status.AWAITING_STAFF
+    )
     closed_reason = models.CharField(
-        max_length=32, choices=CLOSED_REASONS, null=True, blank=True
+        max_length=32, choices=ClosedReason.choices, null=True, blank=True
     )
     closed_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -110,8 +98,8 @@ class Ticket(models.Model):
                 {"assigned_to": "Tickets can only be assigned to staff."}
             )
 
-        if self.status == self.CLOSED[0]:
-            if not self.closed_at:
+        if self.status == self.Status.CLOSED:
+            if self.closed_at is None:
                 self.closed_at = timezone.now()
             if not self.closed_reason:
                 raise ValidationError(
