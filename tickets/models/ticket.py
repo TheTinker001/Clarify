cd1fda50eaa66830a0cf047ef8cd2cfe711bead2
Patrick Dunham <patrick.dunham@kcl.ac.uk>
2026-01-31 02:29:32 +0000

    Create Ticket model and add tests

--- tickets/models/ticket.py
+++ tickets/models/ticket.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.core.validators import MaxLengthValidator
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
+from django.core.validators import MaxLengthValidator
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -8,67 +10,123 @@ User = get_user_model()
 class Ticket(models.Model):
     """Model representing a student ticket."""
 
-    FACULTIES = [
-        "Faculty of Life Sciences & Medicine (FoLSM)",
-        "Faculty of Social Science & Public Policy (SSPP)",
+    # FACULTIES
+    FOLSM = ("folsm", "Faculty of Life Sciences & Medicine (FoLSM)")
+    SSPP = ("sspp", "Faculty of Social Science & Public Policy (SSPP)")
+    NMPC = (
+        "nmpc",
         "Florence Nightingale Faculty of Nursing, Midwifery & Palliative Care (NMPC)",
-        "Faculty of Natural, Mathematical & Engineering Sciences (NMES)",
-        "Faculty of Arts & Humanities (A&H)",
-        "King's Business School (KBS)",
-        "Faculty of Dentistry, Oral & Craniofacial Sciences (DOCS)",
-        "The Dickson Poon School of Law (DPSoL)",
-        "Institute of Psychiatry, Psychology & Neuroscience (IoPPN)",
-    ]
+    )
+    NMES = ("nmes", "Faculty of Natural, Mathematical & Engineering Sciences (NMES)")
+    AH = ("ah", "Faculty of Arts & Humanities (A&H)")
+    KBS = ("kbs", "King's Business School (KBS)")
+    DOCS = ("docs", "Faculty of Dentistry, Oral & Craniofacial Sciences (DOCS)")
+    DPSOL = ("dpsol", "The Dickson Poon School of Law (DPSoL)")
+    IOPPN = ("ioppn", "Institute of Psychiatry, Psychology & Neuroscience (IoPPN)")
+    FACULTIES = [FOLSM, SSPP, NMPC, NMES, AH, KBS, DOCS, DPSOL, IOPPN]
 
-    STUDY_LEVEL = ["Undergraduate", "Postgraduate Taught", "Postgraduate"]
+    # STUDY LEVEL
+    UNDERGRADUATE = ("undergraduate", "Undergraduate")
+    POSTGRADUATE_TAUGHT = ("postgraduate_taught", "Postgraduate Taught")
+    POSTGRADUATE = ("postgraduate", "Postgraduate")
+    STUDY_LEVEL = [UNDERGRADUATE, POSTGRADUATE_TAUGHT, POSTGRADUATE]
 
-    CATEGORIES = [
-        "Assessment",
-        "Welfare",
-        "Health",
-        "Careers",
-        "Financial Issues",
+    # CATEGORIES
+    ASSESSMENT = ("assessment", "Assessment")
+    WELFARE = ("welfare", "Welfare")
+    HEALTH = ("health", "Health")
+    CAREERS = ("careers", "Careers")
+    FINANCIAL_ISSUES = ("financial_issues", "Financial Issues")
+    UNI_PROCEDURES_REGULATIONS = (
+        "uni_procedures_regulations",
         "University Procedures and Regulations",
-        "Misconduct Allegations",
-        "Other",
+    )
+    MISCONDUCT_ALLEGATIONS = ("misconduct_allegations", "Misconduct Allegations")
+    OTHER = ("other", "Other")
+    CATEGORIES = [
+        ASSESSMENT,
+        WELFARE,
+        HEALTH,
+        CAREERS,
+        FINANCIAL_ISSUES,
+        UNI_PROCEDURES_REGULATIONS,
+        MISCONDUCT_ALLEGATIONS,
+        OTHER,
     ]
 
-    STATUS = ["Awaiting staff", "Awaiting student", "Closed"]
-    CLOSED_REASONS = ["None", "Answered", "Inactivity"]
+    # STATUS
+    AWAITING_STAFF = ("awaiting_staff", "Awaiting staff")
+    AWAITING_STUDENT = ("awaiting_student", "Awaiting student")
+    CLOSED = ("closed", "Closed")
+    STATUS = [AWAITING_STAFF, AWAITING_STUDENT, CLOSED]
 
-    """
+    # CLOSED REASONS
+    ANSWERED = ("answered", "Answered")
+    INACTIVITY = ("inactivity", "Inactivity")
+    CLOSED_REASONS = [ANSWERED, INACTIVITY]
+
     student = models.ForeignKey(
-        User.USER_TYPE_STUDENT, on_delete=models.CASCADE, related_name="tickets"
+        User,
+        on_delete=models.CASCADE,
+        related_name="tickets",
+        limit_choices_to={"user_type": User.USER_TYPE_STUDENT},
     )
-    """
-    faculty = models.CharField(
-        max_length=100, choices=[(fac, fac) for fac in FACULTIES]
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="assigned_tickets",
+        limit_choices_to={"user_type": User.USER_TYPE_STAFF},
     )
-    study_level = models.CharField(
-        max_length=100, choices=[(level, level) for level in STUDY_LEVEL]
-    )
-    category = models.CharField(
-        max_length=100, choices=[(cat, cat) for cat in CATEGORIES]
-    )
+
+    faculty = models.CharField(max_length=64, choices=FACULTIES)
+    study_level = models.CharField(max_length=64, choices=STUDY_LEVEL)
+    category = models.CharField(max_length=64, choices=CATEGORIES)
+
     subject = models.CharField(max_length=78)
-    body = models.TextField(validators=[MaxLengthValidator(50000)])
 
-    status = models.CharField(
-        max_length=100, choices=[(stat, stat) for stat in STATUS], default=STATUS[0]
+    BODY_MAX_LENGTH = 50000
+    body = models.TextField(
+        max_length=BODY_MAX_LENGTH, validators=[MaxLengthValidator(BODY_MAX_LENGTH)]
     )
+
+    status = models.CharField(max_length=32, choices=STATUS, default=AWAITING_STAFF[0])
     closed_reason = models.CharField(
-        max_length=100,
-        choices=[(reason, reason) for reason in CLOSED_REASONS],
-        default=CLOSED_REASONS[0],
+        max_length=32, choices=CLOSED_REASONS, null=True, blank=True
     )
+    closed_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def clean(self):
+        super().clean()
 
-"""
-TODO
+        if self.student and self.student.user_type != User.USER_TYPE_STUDENT:
+            raise ValidationError({"student": "Ticket can only be made by students."})
 
-- wait for Gor code
-- test Gor code in browser
-- see if what I have for student works by creating tickets in the shell
-- add tests similar to user tests
+        if self.assigned_to and self.assigned_to.user_type != User.USER_TYPE_STAFF:
+            raise ValidationError(
+                {"assigned_to": "Tickets can only be assigned to staff."}
+            )
 
-"""
+        if self.status == self.CLOSED[0]:
+            if not self.closed_at:
+                self.closed_at = timezone.now()
+            if not self.closed_reason:
+                raise ValidationError(
+                    {"closed_reason": "Tickets need a reason for closing."}
+                )
+        else:
+            self.closed_at = None
+            self.closed_reason = None
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Ticket {self.pk} | {self.subject}"
+
+    class Meta:
+        ordering = ["-created_at"]
