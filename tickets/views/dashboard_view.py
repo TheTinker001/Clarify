5698105909a04b79be6939b5f526de3daedae7ed
Darren Guan <guandarren1705@gmail.com>
2026-02-04 15:54:01 +0000

    Split ticket queries into seperate contexts

--- tickets/views/dashboard_view.py
+++ tickets/views/dashboard_view.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from tickets.models import Ticket
 from tickets.models import User
+from django.utils import timezone
+from datetime import timedelta
 
 
 @login_required
@@ -15,9 +17,45 @@ def dashboard(request):
     page.
     """
     current_user = request.user
-    if current_user.user_type == User.USER_TYPE_STAFF:
-        tickets = Ticket.objects.all()
-    elif current_user.user_type == User.USER_TYPE_STUDENT:
-        tickets = Ticket.objects.filter(student=current_user)
+    context = {}
 
-    return render(request, "dashboard.html", {"user": current_user, "tickets": tickets})
+    if current_user.user_type == User.USER_TYPE_STAFF:
+        tickets = Ticket.objects.all().order_by("-created_at")
+        overdue_cutoff = timezone.now() - timedelta(days=5)
+
+        context.update(
+            {
+                "open_tickets": tickets.filter(
+                    assigned_to__isnull=True,
+                    status__in=[
+                        Ticket.Status.AWAITING_STAFF,
+                        Ticket.Status.AWAITING_STUDENT,
+                    ],
+                ),
+                "assigned_tickets": tickets.filter(assigned_to=current_user),
+                "overdue_tickets": tickets.filter(
+                    created_at__lt=overdue_cutoff
+                ).exclude(status=Ticket.Status.CLOSED),
+                "closed_tickets": tickets.filter(status=Ticket.Status.CLOSED),
+                "role": "staff",
+            }
+        )
+    elif current_user.user_type == User.USER_TYPE_STUDENT:
+        tickets = Ticket.objects.filter(student=current_user).order_by("-created_at")
+        context.update(
+            {
+                "open_tickets": tickets.filter(
+                    assigned_to__isnull=True,
+                ),
+                "in_progress_tickets": tickets.filter(
+                    assigned_to__isnull=False,
+                ),
+                "need_response_tickets": tickets.filter(
+                    status=Ticket.Status.AWAITING_STUDENT
+                ),
+                "closed_tickets": tickets.filter(status=Ticket.Status.CLOSED),
+                "role": "student",
+            }
+        )
+
+    return render(request, "dashboard.html", context)
