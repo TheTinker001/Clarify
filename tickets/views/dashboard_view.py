6722eb6f251d67031cc14847ddf2004faf3317e5
Darren Guan <guandarren1705@gmail.com>
2026-02-04 17:18:36 +0000

    Add tabs to select what tickets to view

--- tickets/views/dashboard_view.py
+++ tickets/views/dashboard_view.py
@@ -17,45 +17,39 @@ def dashboard(request):
     page.
     """
     current_user = request.user
-    context = {}
+    tab = request.GET.get("tab", "open_tickets")
 
     if current_user.user_type == User.USER_TYPE_STAFF:
         tickets = Ticket.objects.all().order_by("-created_at")
         overdue_cutoff = timezone.now() - timedelta(days=5)
 
-        context.update(
-            {
-                "open_tickets": tickets.filter(
-                    assigned_to__isnull=True,
-                    status__in=[
-                        Ticket.Status.AWAITING_STAFF,
-                        Ticket.Status.AWAITING_STUDENT,
-                    ],
-                ),
-                "assigned_tickets": tickets.filter(assigned_to=current_user),
-                "overdue_tickets": tickets.filter(
-                    created_at__lt=overdue_cutoff
-                ).exclude(status=Ticket.Status.CLOSED),
-                "closed_tickets": tickets.filter(status=Ticket.Status.CLOSED),
-                "role": "staff",
-            }
-        )
+        groups = {
+            "open_tickets": tickets.filter(
+                assigned_to__isnull=True,
+                status__in=[
+                    Ticket.Status.AWAITING_STAFF,
+                    Ticket.Status.AWAITING_STUDENT,
+                ],
+            ),
+            "assigned_tickets": tickets.filter(assigned_to=current_user),
+            "overdue_tickets": tickets.filter(created_at__lt=overdue_cutoff).exclude(
+                status=Ticket.Status.CLOSED
+            ),
+            "closed_tickets": tickets.filter(status=Ticket.Status.CLOSED),
+        }
     elif current_user.user_type == User.USER_TYPE_STUDENT:
         tickets = Ticket.objects.filter(student=current_user).order_by("-created_at")
-        context.update(
-            {
-                "open_tickets": tickets.filter(
-                    assigned_to__isnull=True,
-                ),
-                "in_progress_tickets": tickets.filter(
-                    assigned_to__isnull=False,
-                ),
-                "need_response_tickets": tickets.filter(
-                    status=Ticket.Status.AWAITING_STUDENT
-                ),
-                "closed_tickets": tickets.filter(status=Ticket.Status.CLOSED),
-                "role": "student",
-            }
-        )
+        groups = {
+            "open_tickets": tickets.filter(
+                assigned_to__isnull=True,
+            ),
+            "in_progress_tickets": tickets.filter(
+                assigned_to__isnull=False,
+            ),
+            "need_response_tickets": tickets.filter(
+                status=Ticket.Status.AWAITING_STUDENT
+            ),
+            "closed_tickets": tickets.filter(status=Ticket.Status.CLOSED),
+        }
 
-    return render(request, "dashboard.html", context)
+    return render(request, "dashboard.html", {"tickets": groups[tab]})
