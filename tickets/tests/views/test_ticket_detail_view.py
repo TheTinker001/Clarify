68d9328d3bdc82910d32361d4e80b16187c26c59
0chenhanyen0 <k23058600@kcl.ac.uk>
2026-02-07 01:34:22 +0000

    Add claim to ticket_detail

--- tickets/tests/views/test_ticket_detail_view.py
+++ tickets/tests/views/test_ticket_detail_view.py
@@ -49,3 +49,61 @@ class TicketDetailViewTestCase(TestCase, MenuTesterMixin):
         self.client.login(username=self.student.username, password="Password123")
         response = self.client.get("/ticket/9999/")
         self.assertEqual(response.status_code, 404)
+
+    def test_ticket_claim_and_unclaim(self):
+        self.client.login(username=self.staff.username, password="Password123")
+        claim_url = reverse("ticket_claim", args=[self.ticket.pk])
+        response = self.client.post(claim_url, follow=True)
+        self.ticket.refresh_from_db()
+        self.assertEqual(self.ticket.assigned_to, self.staff)
+        self.assertContains(response, "You have claimed this ticket.")
+
+        unclaim_url = reverse("ticket_unclaim", args=[self.ticket.pk])
+        response = self.client.post(unclaim_url, follow=True)
+        self.ticket.refresh_from_db()
+        self.assertIsNone(self.ticket.assigned_to)
+        self.assertContains(response, "You have unclaimed this ticket.")
+
+    def test_ticket_claim_and_unclaim_by_non_staff(self):
+        self.client.login(username=self.student.username, password="Password123")
+        claim_url = reverse("ticket_claim", args=[self.ticket.pk])
+        response = self.client.post(claim_url, follow=True)
+        self.ticket.refresh_from_db()
+        self.assertNotEqual(self.ticket.assigned_to, self.student)
+        self.assertContains(response, "You are not a staff member!")
+
+        unclaim_url = reverse("ticket_unclaim", args=[self.ticket.pk])
+        response = self.client.post(unclaim_url, follow=True)
+        self.ticket.refresh_from_db()
+        self.assertNotEqual(self.ticket.assigned_to, self.student)
+        self.assertContains(response, "You are not a staff member!")
+
+    def test_ticket_claim_already_claimed(self):
+        other_staff = User.objects.create_user(
+            username="@otherstaff",
+            password="Password123",
+            user_type=User.USER_TYPE_STAFF,
+        )
+        self.ticket.assigned_to = other_staff
+        self.ticket.save()
+        self.client.login(username=self.staff.username, password="Password123")
+        claim_url = reverse("ticket_claim", args=[self.ticket.pk])
+        response = self.client.post(claim_url, follow=True)
+        self.ticket.refresh_from_db()
+        self.assertEqual(self.ticket.assigned_to, other_staff)
+        self.assertContains(response, f"Ticket already claimed by {other_staff}.")
+
+    def test_ticket_unclaim_not_assigned(self):
+        other_staff = User.objects.create_user(
+            username="@otherstaff",
+            password="Password123",
+            user_type=User.USER_TYPE_STAFF,
+        )
+        self.ticket.assigned_to = other_staff
+        self.ticket.save()
+        self.client.login(username=self.staff.username, password="Password123")
+        unclaim_url = reverse("ticket_unclaim", args=[self.ticket.pk])
+        response = self.client.post(unclaim_url, follow=True)
+        self.ticket.refresh_from_db()
+        self.assertEqual(self.ticket.assigned_to, other_staff)
+        self.assertContains(response, f"You are not assigned to this ticket.")
--- tickets/views/ticket_detail_view.py
+++ tickets/views/ticket_detail_view.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
 
 from tickets.models import Ticket
+from django.contrib import messages
 
 
 @login_required
@@ -11,4 +12,40 @@ def ticket_detail(request, pk):
         Ticket.objects.select_related("student", "assigned_to"),
         pk=pk,
     )
+    return render(request, "ticket_detail.html", {"ticket": ticket})
+
+
+@login_required
+def ticket_claim(request, ticket_id):
+    ticket = get_object_or_404(Ticket, pk=ticket_id)
+    if request.user.user_type != "staff":
+        messages.add_message(request, messages.ERROR, "You are not a staff member!")
+        return render(request, "ticket_detail.html", {"ticket": ticket})
+    if ticket.assigned_to and ticket.assigned_to != request.user:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            f"Ticket already claimed by {ticket.assigned_to}.",
+        )
+        return render(request, "ticket_detail.html", {"ticket": ticket})
+    ticket.assigned_to = request.user
+    ticket.save()
+    messages.add_message(request, messages.SUCCESS, "You have claimed this ticket.")
+    return render(request, "ticket_detail.html", {"ticket": ticket})
+
+
+@login_required
+def ticket_unclaim(request, ticket_id):
+    ticket = get_object_or_404(Ticket, pk=ticket_id)
+    if request.user.user_type != "staff":
+        messages.add_message(request, messages.ERROR, "You are not a staff member!")
+        return render(request, "ticket_detail.html", {"ticket": ticket})
+    if not ticket.assigned_to or ticket.assigned_to != request.user:
+        messages.add_message(
+            request, messages.ERROR, f"You are not assigned to this ticket."
+        )
+        return render(request, "ticket_detail.html", {"ticket": ticket})
+    ticket.assigned_to = None
+    ticket.save()
+    messages.add_message(request, messages.SUCCESS, "You have unclaimed this ticket.")
     return render(request, "ticket_detail.html", {"ticket": ticket})
