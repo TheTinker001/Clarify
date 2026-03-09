d7286c876b90041029345e28661592f89e3507b3
Patrick Dunham <patrick.dunham@kcl.ac.uk>
2026-03-09 01:01:38 +0000

    Add models to 'admin.py'

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from tickets.models import Ticket
+from tickets.models import *
 
 admin.site.register(Ticket)
+admin.site.register(User)
+admin.site.register(TicketAttachment)
+admin.site.register(Comment)
+admin.site.register(InternalNote)
