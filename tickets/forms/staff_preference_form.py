bb938f5facdf72cd01c24a094d2600f30f8cbeb8
Gor (Gor) Vardanyan <gor.vardanyan@kcl.ac.uk>
2026-03-09 16:20:18 +0000

    Updated everything up to the new main

--- tickets/forms/staff_preference_form.py
+++ tickets/forms/staff_preference_form.py
@@ -38,12 +38,19 @@ class StaffPreferenceForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        if self.instance.faculties:
-            self.initial["faculties"] = _split_codes(self.instance.faculties)
-        if self.instance.study_levels:
-            self.initial["study_levels"] = _split_codes(self.instance.study_levels)
-        if self.instance.categories:
-            self.initial["categories"] = _split_codes(self.instance.categories)
+        all_faculties = [code for code, _ in _no_empty(Ticket.Faculty.choices)]
+        all_study_levels = [code for code, _ in _no_empty(Ticket.StudyLevel.choices)]
+        all_categories = [code for code, _ in _no_empty(Ticket.Category.choices)]
+
+        self.initial["faculties"] = (
+            _split_codes(self.instance.faculties) if self.instance.faculties else all_faculties
+        )
+        self.initial["study_levels"] = (
+            _split_codes(self.instance.study_levels) if self.instance.study_levels else all_study_levels
+        )
+        self.initial["categories"] = (
+            _split_codes(self.instance.categories) if self.instance.categories else all_categories
+        )
 
     def clean(self):
         cleaned_data = super().clean()
--- tickets/tests/views/test_staff_preferences_view.py
+++ tickets/tests/views/test_staff_preferences_view.py
@@ -127,6 +127,29 @@ class StaffPreferencesViewTestCase(TestCase):
             [Ticket.Category.WELFARE, Ticket.Category.UNI_PROCEDURES_REGULATIONS],
         )
 
+    def test_all_options_selected_by_default_for_new_staff(self):
+        self.client.login(username=self.staff.username, password="Password123")
+        response = self.client.get(self.url)
+        form = response.context["form"]
+        all_faculties = [code for code, _ in Ticket.Faculty.choices if code]
+        all_study_levels = [code for code, _ in Ticket.StudyLevel.choices if code]
+        all_categories = [code for code, _ in Ticket.Category.choices if code]
+        self.assertEqual(form.initial["faculties"], all_faculties)
+        self.assertEqual(form.initial["study_levels"], all_study_levels)
+        self.assertEqual(form.initial["categories"], all_categories)
+
+    def test_saved_preferences_shown_instead_of_defaults(self):
+        self.staff.faculties = "kbs,nmes"
+        self.staff.study_levels = "undergraduate"
+        self.staff.categories = "welfare"
+        self.staff.save()
+        self.client.login(username=self.staff.username, password="Password123")
+        response = self.client.get(self.url)
+        form = response.context["form"]
+        self.assertEqual(form.initial["faculties"], ["kbs", "nmes"])
+        self.assertEqual(form.initial["study_levels"], ["undergraduate"])
+        self.assertEqual(form.initial["categories"], ["welfare"])
+
     def test_staff_preferences_form_excludes_select_placeholder_choice(self):
         self.client.login(username=self.staff.username, password="Password123")
         response = self.client.get(self.url)
