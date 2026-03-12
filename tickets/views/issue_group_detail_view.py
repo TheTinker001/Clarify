b231283c85561ddc3dd04cf04ef923a1770d4b8a
Darren Guan <guandarren1705@gmail.com>
2026-03-12 00:34:56 +0000

    Add paginator for issue groups and issue group tickets

--- tickets/views/issue_group_detail_view.py
+++ tickets/views/issue_group_detail_view.py
@@ -2,6 +2,9 @@ from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from tickets.models import User, IssueGroup
+from django.core.paginator import Paginator
+
+from clarify.settings import ITEMS_PER_PAGE
 
 
 class IssueGroupDetailView(LoginRequiredMixin, TemplateView):
@@ -17,4 +20,12 @@ class IssueGroupDetailView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["issue_group"] = self.issue_group
+
+        paginator = Paginator(self.issue_group.tickets.all(), ITEMS_PER_PAGE)
+        page_number = self.request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
+        context["tickets"] = page_obj
+        context["page_obj"] = page_obj
+        context["paginator"] = paginator
         return context
