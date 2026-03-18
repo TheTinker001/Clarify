c4530e49e0d844f13bac7f6bb28a52ab5bf5a728
Darren Guan <guandarren1705@gmail.com>
2026-03-18 22:19:28 +0000

    Add filter of all, open and closed tickets in issue group details view

--- tickets/views/issue_group_detail_view.py
+++ tickets/views/issue_group_detail_view.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from tickets.models import User, IssueGroup
+from tickets.models import User, IssueGroup, Ticket
 from django.core.paginator import Paginator
 
 from clarify.settings import ITEMS_PER_PAGE
@@ -22,8 +22,9 @@ class IssueGroupDetailView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["issue_group"] = self.issue_group
+        qs = self.get_qs(context)
 
-        paginator = Paginator(self.issue_group.tickets.all(), ITEMS_PER_PAGE)
+        paginator = Paginator(qs, ITEMS_PER_PAGE)
         page_number = self.request.GET.get("page")
         page_obj = paginator.get_page(page_number)
 
@@ -31,3 +32,23 @@ class IssueGroupDetailView(LoginRequiredMixin, TemplateView):
         context["page_obj"] = page_obj
         context["paginator"] = paginator
         return context
+
+    def get_qs(self, context, **kwargs):
+        (open_qs, closed_qs) = self.get_query_set()
+        status = self.request.GET.get("status", "")
+        if status == "open":
+            return_qs = open_qs
+            context["status"] = "open"
+        elif status == "closed":
+            return_qs = closed_qs
+            context["status"] = "closed"
+        else:
+            return_qs = open_qs | closed_qs
+            context["status"] = ""
+        return return_qs.order_by("subject")
+
+    def get_query_set(self, **kwargs):
+        qs = self.issue_group.tickets.all()
+        open_qs = qs.exclude(status=Ticket.Status.CLOSED)
+        closed_qs = qs.filter(status=Ticket.Status.CLOSED)
+        return (open_qs, closed_qs)
