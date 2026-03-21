ccbcc916a1403733431bc6a57879da424475808b
Patrick Dunham <patrick.dunham@kcl.ac.uk>
2026-03-21 03:58:12 +0000

    Edit 'tickets/views/dashboard_view.py'

--- tickets/views/dashboard_view.py
+++ tickets/views/dashboard_view.py
@@ -5,9 +5,7 @@ from django.views.generic import TemplateView
 from django.db.models import Q, Value, Count
 from django.db.models.functions import Concat
 from tickets.models import Ticket, User
-
 from datetime import timedelta
-
 from clarify.settings import ITEMS_PER_PAGE
 
 
