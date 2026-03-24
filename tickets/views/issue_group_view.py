44a9b0d60a7f9d6c329b7fffeece025c7c551386
Patrick Dunham <patrick.dunham@kcl.ac.uk>
2026-03-24 14:53:03 +0000

    Add pagination to '/issues'

--- tickets/views/issue_group_view.py
+++ tickets/views/issue_group_view.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import Http404
-from django.views.generic import TemplateView
-from tickets.models import User, IssueGroup
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.http import Http404
+from django.views.generic import TemplateView
 
 from clarify.settings import ITEMS_PER_PAGE
+from tickets.helpers import get_page_slots
+from tickets.models import User, IssueGroup
 
 
 class IssueGroupView(LoginRequiredMixin, TemplateView):
@@ -29,9 +30,25 @@ class IssueGroupView(LoginRequiredMixin, TemplateView):
         page_number = self.request.GET.get("page")
         page_obj = paginator.get_page(page_number)
 
-        context["issue_groups"] = page_obj
-        context["page_obj"] = page_obj
-        context["paginator"] = paginator
+        cur = page_obj.number
+        max_pages = paginator.num_pages
+        page_slots = get_page_slots(cur, max_pages)
+
+        params = self.request.GET.copy()
+        params.pop("page", None)
+        querystring = params.urlencode()
+
+        context.update(
+            {
+                "issue_groups": page_obj,
+                "page_obj": page_obj,
+                "paginator": paginator,
+                "cur": cur,
+                "max_pages": max_pages,
+                "page_slots": page_slots,
+                "querystring": querystring,
+            }
+        )
 
         return context
 
