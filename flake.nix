bdcd8dca3d258189bd92d710745235916ae2c7ee
Patrick Dunham <patrick.dunham@kcl.ac.uk>
2026-03-25 14:48:21 +0000

    Ensure 'flake.nix' is correctly setup

--- flake.nix
+++ flake.nix
@@ -76,7 +76,30 @@
           PY
         '';
 
-        # Core Python environment for running the app + tests.
+        djangoSummernote = python.pkgs.buildPythonPackage rec {
+          pname = "django-summernote";
+          version = "0.8.20.0";
+
+          pyproject = true;
+
+          src = pkgs.fetchPypi {
+            inherit pname version;
+            hash = "sha256-UumxJDjtnqwNd3KfdY8qrgbkaLXLzhM+JBANWK5OQ6g=";
+          };
+
+          build-system = with python.pkgs; [
+            setuptools
+          ];
+
+          dependencies = with python.pkgs; [
+            django
+            bleach
+          ];
+
+          pythonImportsCheck = [ "django_summernote" ];
+          doCheck = false;
+        };
+
         pythonEnv = python.withPackages (ps: [
           ps.django
           ps.coverage
@@ -86,7 +109,7 @@
           ps.pillow
           ps."python-dotenv"
           ps."django-widget-tweaks"
-          ps."django-summernote"
+          djangoSummernote
           ps.libgravatar
         ]);
 
